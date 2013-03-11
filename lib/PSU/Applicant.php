c42a42413a57b9ce9ac353dd557c91d5b6d97c3a
Nathan Porter <nrporter1@gmail.com>
2013-03-11 15:57:50 -0400

    Fixing class variable call

--- lib/PSU/Applicant.php
+++ lib/PSU/Applicant.php
@@ -38,7 +38,7 @@ class Applicant {
 		$func = '_load_' . $key;
 		if( method_exists( $this, $func ) ) {
 			return $this->$func();
-		} elseif( in_array( $key , array_keys( $loaders ) ) ) {
+		} elseif( in_array( $key , array_keys( $this->loaders ) ) ) {
 			$func = '_load_' . $loaders[ $key ];
 			$this->$func();
 			return $this->data[ $key ];
