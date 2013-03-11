1a035b2d6843208cc87c5e9069eea7d1fea9930b
Nathan Porter <nrporter1@gmail.com>
2013-03-11 15:05:14 -0400

    Adding another )

--- lib/PSU/Applicant.php
+++ lib/PSU/Applicant.php
@@ -31,7 +31,7 @@ class Applicant {
 
 	public function &__get( $key ) {
 
-		if( isset( $this->data[ $key ] ) {
+		if( isset( $this->data[ $key ] ) ) {
 			return $this->data[ $key ];
 		}//end if
 
